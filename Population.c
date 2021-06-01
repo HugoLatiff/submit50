5b6d53a1d37b1f1dcc5913d35e8a6b8259fbcffa
HugoLatiff <60037199+HugoLatiff@users.noreply.github.com>
2021-06-01 15:43:04 +0200

    Add files via upload

---
+++ Population.c
@@ -0,0 +1,32 @@
+#include <cs50.h>
+#include <stdio.h>
+int main(void)
+{
+  int n,#,y,b,d; 
+   // TODO: Prompt for start size
+do
+{
+    /* n = get_int("start size:");
+
+    while (n < 9);
+} while (/* condition */);
+
+    // TODO: Prompt for end size
+    do
+    {
+       n = get_int("End Size:"); 
+    } while (n < n);
+    
+
+    // TODO: Calculate number of years until we reach threshold
+for (y = 0; n < n; y++)
+{
+  b = n/3;
+  d = n/4;
+  n = n + b - d;
+}
+ 
+    {
+        printf("years: %i\n", y);
+    }
+}
